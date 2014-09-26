15d74f90f1912fcc7c16638fcb5e30b3cb5ce364
Daniel Furze <daniel@furzeface.com>
2014-09-26 10:50:24 +0100

    feat(Gruntfile): Adds jshintrc

--- test/combine-mq_test.js
+++ test/combine-mq_test.js
@@ -1,16 +1,1 @@
 'use strict';
-
-// var assert = require('assert');
-
-// var task = require('../lib/combine-mq');
-
-// describe('Combine Media Queries', function() {
-//   describe('getFile', function() {
-//     it('should return a file with contents', function() {
-
-//       var file = task.getFile('./test/examples/test.css');
-      
-//       assert.equal(file, 'combineMq'); 
-//     });
-//   });
-// }); 
