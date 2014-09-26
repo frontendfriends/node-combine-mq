ebb72f65babad975554275ee54eca1f88cdfb5d7
Daniel Furze <daniel@furzeface.com>
2014-09-26 11:31:59 +0100

    fix(Task): Fixes task when no charset or document provided

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,77 +8,83 @@ module.exports = function (grunt) {
 
   // Project configuration
   grunt.initConfig({
-    nodeunit: {
-      files: ['test/**/*_test.js']
-    },
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'nyan'
-        },
-        src: [
-        'test/**/*.js'
-        ]
-      }
-    },
-    jshint: {
-      options: {
-      	jshintrc: true,
-        reporter: require('jshint-stylish')
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib: {
-        src: [
-        'lib/**/*.js'
-        ]
-      },
-      test: {
-        src: [
-        'test/**/*.js'
-        ]
-      }
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: [
-          'jshint:gruntfile'
-        ]
-      },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: [
-          'jshint:lib',
-          'mochaTest',
-        ]
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: [
-          'jshint:test',
-          'mochaTest',
-        ]
-        }
-      }
-    });
+  	config: {
+  		gruntfile: 'Gruntfile.js'
+  	},
+
+  	nodeunit: {
+  		files: [
+  		'test/**/*_test.js'
+  		]
+  	},
+  	mochaTest: {
+  		test: {
+  			options: {
+  				reporter: 'nyan'
+  			},
+  			src: [
+  			'test/**/*.js'
+  			]
+  		}
+  	},
+  	jshint: {
+  		options: {
+  			jshintrc: true,
+  			reporter: require('jshint-stylish')
+  		},
+  		gruntfile: {
+  			src: '<%= config.gruntfile %>'
+  		},
+  		lib: {
+  			src: [
+  			'lib/**/*.js'
+  			]
+  		},
+  		test: {
+  			src: [
+  			'test/**/*.js'
+  			]
+  		}
+  	},
+  	watch: {
+  		gruntfile: {
+  			files: '<%= jshint.gruntfile.src %>',
+  			tasks: [
+  			'jshint:gruntfile'
+  			]
+  		},
+  		lib: {
+  			files: '<%= jshint.lib.src %>',
+  			tasks: [
+  			'jshint:lib',
+  			'mochaTest',
+  			]
+  		},
+  		test: {
+  			files: '<%= jshint.test.src %>',
+  			tasks: [
+  			'jshint:test',
+  			'mochaTest'
+  			]
+  		}
+  	}
+  });
 
   // Default task
   grunt.registerTask('default', [
-    'dev'
-    ]);
+  	'dev'
+  	]);
 
   // Dev task
   grunt.registerTask('dev', [
-    'jshint',
-    'mochaTest',
-    'watch'
-    ]);
+  	'jshint',
+  	'mochaTest',
+  	'watch'
+  	]);
 
   // Test task
   grunt.registerTask('test', [
-    'jshint',
-    'mochaTest'
-    ]);
+  	'jshint',
+  	'mochaTest'
+  	]);
 };
--- cli.js
+++ cli.js
@@ -2,17 +2,17 @@
 
 'use strict';
 
- var task = require('./lib/combine-mq'),
-     packageJson = require('./package'),
-     program = require('commander');
+var task = require('./lib/combine-mq'),
+packageJson = require('./package'),
+program = require('commander');
 
- program.version(packageJson.version);
+program.version(packageJson.version);
 
- program
- .command('combine <fileName>')
- .description('Description...')
- .action(function (fileName) {
-   task.init(fileName);
+program
+.command('combine <fileName>')
+.description('Description...')
+.action(function (fileName) {
+	task.init(fileName);
 });
 
 program.parse(process.argv);
